f48551cdfd3fb300f26579e82e9e6b5c7b5a5232
Foul Tarnished <ammarhassan.psca@gmail.com>
2024-07-29 14:11:12 +0500

    Add bento grid item styling implementation

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Hero from "@/components/Hero";
+import { FaHome } from "react-icons/fa";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import Grid from "@/components/Grid";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[
+          {name: 'Home', link: '/', icon: <FaHome />}
+        ]}  />
+        <Hero />
+        <Grid />
+      </div>
+    </main>
+  );
+}
